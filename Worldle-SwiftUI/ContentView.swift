d8ff5dc8b4504eef8e8ad1b098c53f25ba636071
hernirg <hernanrg85@gmail.com>
2024-08-25 16:17:08 +0200

    Initial Commit

---
+++ Worldle-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Worldle-SwiftUI
+//
+//  Created by Hernán Rodríguez on 25/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Worldle-SwiftUI/Worldle_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Worldle_SwiftUIApp.swift
+//  Worldle-SwiftUI
+//
+//  Created by Hernán Rodríguez on 25/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Worldle_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
